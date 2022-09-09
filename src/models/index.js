6530207c26d6ecbdefd043c0c3ddb215a0bdbb5c
Ibsy Matute <ibsy@rocketmail.com>
2022-09-09 11:22:41 -0500

    added quiz and question models

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,6 @@
 // to get all models out we need this index.js
 module.exports = {
-    User: require('./user.model')
+    User: require('./user.model'),
+    Quiz: require('./quiz.model'),
+    Question: require('./question.model')
 }
