b860b3e01cb067a175d7d15c5fce83dd94cc44ff
Ibsy Matute <ibsy@rocketmail.com>
2022-09-09 16:33:25 -0500

    Create quiz route

--- src/routes/quiz/index.js
+++ src/routes/quiz/index.js
@@ -2,5 +2,6 @@ const QuizDashboardRouter = require('express').Router()
 
 QuizDashboardRouter.route("/create")
     .get(require('./editor'))
+    .post(require('./create'))
 
 module.exports = QuizDashboardRouter
